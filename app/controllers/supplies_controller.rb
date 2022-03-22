8cfd13455c658aea3911f7295d0cd9137629e7cb
RSandage <90716032+RSandage@users.noreply.github.com>
2022-03-22 12:17:53 -0500

    Added All CRUD operations, Included links to pages

--- app/controllers/supplies_controller.rb
+++ app/controllers/supplies_controller.rb
@@ -7,4 +7,41 @@ class SuppliesController < ApplicationController
   def index
     @supplies = Supply.all
   end
+
+  def new
+    @supply = Supply.new
+  end
+
+  def create
+    @supply = Supply.new(params.require(:supply).permit(:name, :amount))
+    @supply.save
+    if @supply.save
+      flash[:notice] = "Supply was created successfully."
+      redirect_to @supply
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+    @supply = Supply.find(params[:id])
+  end
+
+  def update
+    @supply = Supply.find(params[:id])
+    @supply.update(params.require(:supply).permit(:name, :amount))
+    if @supply.update(params.require(:supply).permit(:name, :amount))
+      flash[:message] = "Updated Successfully!"
+      redirect_to @supply
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @supply = Supply.find(params[:id])
+    flash[:message] = "#{@supply.name} was successfully deleted."
+    @supply.destroy
+    redirect_to supplies_path
+  end
 end
