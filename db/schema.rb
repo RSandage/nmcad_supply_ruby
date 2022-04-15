67149697d4a43f08a190703b3a1c4f96ca33c76e
RSandage <90716032+RSandage@users.noreply.github.com>
2022-04-14 22:15:54 -0500

    created user and station tables, added password digest to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_13_205235) do
+ActiveRecord::Schema.define(version: 2022_04_14_172015) do
+
+  create_table "stations", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "supplies", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.text "amount"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.string "password_digest"
   end
 
 end
