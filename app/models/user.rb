0f73557349491074105988f47ea80ee8cd23c8dd
RSandage <90716032+RSandage@users.noreply.github.com>
2022-04-18 16:16:09 -0500

    Added password validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,5 @@ class User < ApplicationRecord
   has_many :station
   has_many :supplies, through: :station
   validates :username, uniqueness: true, presence: true, length: {minimum: 4, maximum: 10}
+  validates :password, presence: true, length: {minimum: 6, maximum: 15}
 end
