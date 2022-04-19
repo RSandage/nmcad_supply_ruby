992df49e446e28479a686666bbadeed24eec508a
RSandage <90716032+RSandage@users.noreply.github.com>
2022-04-19 17:09:56 -0500

    Added require_user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
+  def require_user
+    if !helpers.logged_in?
+      flash[:notice] = "You must be logged in to perform this action."
+      redirect_to login_path
+    end
+  end
 
 end
