a619469029709126b2243ede5c7d30abf6c507f7
RSandage <90716032+RSandage@users.noreply.github.com>
2022-04-19 17:12:53 -0500

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_04_14_172015) do
+ActiveRecord::Schema.define(version: 2022_04_19_195303) do
 
   create_table "stations", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 2022_04_14_172015) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "password_digest"
+    t.boolean "admin", default: false
   end
 
 end
