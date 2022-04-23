d8248fb78144a58a4088be0da17325e77c0dc47e
RSandage <90716032+RSandage@users.noreply.github.com>
2022-04-23 11:43:55 -0500

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,18 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_04_19_195303) do
+ActiveRecord::Schema.define(version: 2022_04_23_152043) do
 
   create_table "stations", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "name"
   end
 
   create_table "supplies", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.text "amount"
+    t.integer "amount"
   end
 
   create_table "users", force: :cascade do |t|
