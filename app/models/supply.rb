e14472d5cd45197a27c71d423b2adee4b29fd9c1
RSandage <90716032+RSandage@users.noreply.github.com>
2022-04-23 11:38:39 -0500

    Fixed relationships

--- app/models/supply.rb
+++ app/models/supply.rb
@@ -1,6 +1,8 @@
 class Supply < ApplicationRecord
-  belongs_to :user
-  belongs_to :station
+ 
+  # belongs_to :station
   validates :name, presence:true, length: {minimum: 2, maximum: 25}
   validates :amount, presence:true
+  scope :low_stock, -> { where("amount <= 10")}
+  scope :normal_stock, -> { where("amount > 10")}
 end
