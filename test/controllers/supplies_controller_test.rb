e4f168ac072aeeb913564f7ac4b2c3a8ab332870
RSandage <90716032+RSandage@users.noreply.github.com>
2022-04-23 11:44:08 -0500

    created tests for supplies controller

--- test/controllers/supplies_controller_test.rb
+++ test/controllers/supplies_controller_test.rb
@@ -1,7 +1,22 @@
 require "test_helper"
 
 class SuppliesControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+
+  def setup
+    @supply = Supply.new(name: " ")
+  end 
+
+  test "name should be present" do
+    assert_not @supply.valid?
+  end
+
+  test "name should not be too short" do
+    @supply.name = "X"
+    assert_not @supply.valid?
+  end
+
+  test "name should not be too long" do
+    @supply.name = "abcdefghijklmnopqrstuvwxyz"
+    assert_not @supply.valid?
+  end
 end
